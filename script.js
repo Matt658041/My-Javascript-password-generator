acb1198299f83e9e1c5d0ba6e308592171620dfb
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-02-23 14:50:47 -0600

    if-statement

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Assignment code here
-var randomNumber = function(min, max) {
-    var value = Math.floor(Math.random() * (max -min) + min);
-}
+let generatePassword = function()  {
+    window.alert(`Please select from the following criteria to create a password`);
+};
+
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
